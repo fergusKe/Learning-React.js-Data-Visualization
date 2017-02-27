79a57000e7290640cca4304a9fee62572eed4ae6
fergus <fergus.jingtai@gmail.com>
2017-02-28 03:46:57 +0800

    2. Overview of JSON Basics

--- source/client.js
+++ source/client.js
@@ -1,2 +1,12 @@
+var obj = {
+	"name":"Ben",
+	"age":33,
+	"smart":false,
+	"friends":["Dan", "Sam", "Samanta"],
+	"eyes": {
+		"left":"green",
+		"right":"brown"
+	}
+};
 
 document.body.innerText = "Starter File is working.";
