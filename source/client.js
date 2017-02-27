d052ee8e3f6b08736c27357e51ef40657c3d031c
fergus <fergus.jingtai@gmail.com>
2017-02-28 05:44:55 +0800

    6. Creating a Map

--- source/client.js
+++ source/client.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './components/app';
+import Map from './components/map';
 
-import data from "./data.json";
-
-ReactDom.render(<App {...data} />, document.getElementById('react'));
+ReactDom.render(<Map />, document.getElementById('react'));
---
+++ source/components/map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Map extends React.Component {
+  render() {
+    return (
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Earthmap1000x500compac.jpg" />
+    )
+  }
+}
