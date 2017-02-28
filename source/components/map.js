19b9eb6760a662ad0b8dd6719d92c6fdd713a9f4
fergus <fergus.jingtai@gmail.com>
2017-03-01 01:31:55 +0800

    7. Finding Points on a Map

--- source/components/map.js
+++ source/components/map.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default class Map extends React.Component {
+  onClick(e) {
+    let img = e.target,
+        point = { x: e.offsetX, y: e.offsetY };
+
+    if (!point.x) {
+      point = { x: e.pageX - e.target.offsetLeft,
+                y: e.pageY - e.target.offsetTop };
+    }
+
+    console.log(point);
+  }
+
   render() {
     return (
-      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Earthmap1000x500compac.jpg" />
+      <img onClick={this.onClick} src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Earthmap1000x500compac.jpg" />
     )
   }
 }
