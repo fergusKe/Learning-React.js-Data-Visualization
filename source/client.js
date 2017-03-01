d134a191269151e7a8b76ab7b885e7bcdde04bbd
fergus <fergus.jingtai@gmail.com>
2017-03-02 02:50:26 +0800

    14. Preparing the Chart Data Object

--- source/client.js
+++ source/client.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Chart from './components/chart';
 
-ReactDom.render(<Chart />, document.getElementById('react'));
+const data = {
+  type: "bar",
+  data: {
+    label: ["Ben", "James", "Mary"],
+    dataset: [{
+      label: 'Fun',
+      data: [12, 54, 66],
+      backgroundColor: "rgba(255, 0, 0, .2)",
+      borderWidth: 1
+    }, {
+      label: 'Smart',
+      data: [62, 34, 76],
+      backgroundColor: "rgba(255, 0, 0, .4)",
+      borderWidth: 1
+    }]
+  },
+  option: {
+    
+  }
+}
+
+ReactDom.render(<Chart data={...data} />, document.getElementById('react'));
