6cd27fc971dd1a53269a22b19f9edd3943ec3689
Fergus <fergus.jingtai@gmail.com>
2017-03-02 16:08:59 +0800

    18. Adding Random Colors

--- source/utils/a2chart.js
+++ source/utils/a2chart.js
@@ -1,3 +1,5 @@
+import RandomColor from 'just.randomcolor';
+
 export function a2c(a) {
   let obj = a.shift(),
   		formatedData = {labels: [obj.name], datasets: []},
@@ -5,7 +7,15 @@ export function a2c(a) {
 
   delete obj.name;
   for(let item in obj){
-  	let ds = {label: item,data: [obj.item]};
+  	let ds = {label: item,data: [obj[item]]},
+        clr = new RandomColor().toRGBA();
+
+    ds.borderColor = clr.toCSS();
+    ds.borderWidth = 2;
+
+    clr.value.a = .5;
+    ds.backgroundColor = clr.toCSS();
+
   	formatedData.datasets.push(ds);
   	map[item] = ds;
   }
